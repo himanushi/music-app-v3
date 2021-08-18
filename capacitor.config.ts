b5cde020aea55bd4ce819db996ef46470270db9e
himanushi <himanushi@users.noreply.github.com>
2021-08-19 08:41:12 +0900

    capacitor init

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "io.ionic.starter",
+  appName: "music-app",
+  bundledWebRuntime: false,
+  webDir: "build"
+};
+
+export default config;
