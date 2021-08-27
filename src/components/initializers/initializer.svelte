b2ff748fa11a17114cc19b4755458a0af2305611
himanushi <himanushi@users.noreply.github.com>
2021-08-28 07:29:19 +0900

    不要になった define 初期化を削除

--- src/components/initializers/initializer.svelte
+++ src/components/initializers/initializer.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
 import AppleMusicAccount from "~/components/initializers/apple-music-account.svelte";
 import CookiePolicy from "~/components/initializers/cookie-policy.svelte";
-import DefineComponents from "~/components/initializers/define-components.svelte";
 import Version from "~/components/initializers/version.svelte";
 </script>
 
 <Version />
-<DefineComponents />
 <AppleMusicAccount />
 <CookiePolicy />
