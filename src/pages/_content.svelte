b7ddf4dd143b01cf7ef83d584f8acb06202551c6
himanushi <himanushi@users.noreply.github.com>
2021-08-30 17:21:46 +0900

    fab button 分を padding

--- src/pages/_content.svelte
+++ src/pages/_content.svelte
@@ -6,3 +6,10 @@ import BackButton from "~/components/back-button.svelte";
   <slot />
   <BackButton />
 </ion-content>
+
+<style lang="scss">
+// ion-fab 56px + padding 10px * 2
+ion-content {
+  --padding-bottom: 76px;
+}
+</style>
