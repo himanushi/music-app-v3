8c0bb2316b2f3b14320982444fd7332890d3788d
himanushi <himanushi@users.noreply.github.com>
2021-09-11 20:04:02 +0900

    フッターにラジオ機能追加

--- src/pages/_footer.svelte
+++ src/pages/_footer.svelte
@@ -1,16 +1,14 @@
 <script lang="ts">
 import { fly } from "svelte/transition";
-import { modals } from "~/components/modals.svelte";
 import { canPlay } from "~/components/play-button.svelte";
-import Player, { openPlayer } from "~/components/player.svelte";
+import { openPlayer } from "~/components/player.svelte";
 import SquareImage from "~/components/square-image.svelte";
-import { openModal } from "~/lib/ionic";
 import { playerService } from "~/machines/jukebox-machine";
 import { radioService } from "~/machines/radio-machine";
 
 $: track = $playerService.context.currentTrack;
 $: player = $playerService.context.musicPlayerRef;
-$: disabled = player && $player.value === "loading";
+$: disabled = player && ($player.value === "loading" || !$canPlay);
 
 const play_or_pause = () => {
 
@@ -61,24 +59,46 @@ const showPlayer = () => {
   <ion-toolbar color="main">
     {#if track && player}
       {#key track.id}
-        <ion-thumbnail on:click={showPlayer} slot="start">
-          <SquareImage src={track.artworkM.url} />
-        </ion-thumbnail>
-        <ion-title on:click={showPlayer}>{track.name}</ion-title>
-        <ion-buttons slot="end">
-          <ion-button {disabled} on:click={play_or_pause}>
+        {#if $playerService.context.isRadio}
+          <ion-thumbnail on:click={showPlayer} slot="start">
+            <SquareImage src={track.artworkM.url} />
+          </ion-thumbnail>
+          <ion-title on:click={showPlayer}>{track.name}</ion-title>
+          <ion-buttons slot="end">
             {#if $player.value === "playing"}
-              <ion-icon name="pause" />
-            {:else if $player.value === "loading" || $player.value === "playerSelecting"}
-              <ion-icon name="sync" />
+              <ion-button {disabled} on:click={stop}>
+                <ion-icon name="stop" />
+              </ion-button>
+            {:else if $player.value === "loading"}
+              <ion-button disabled={true}>
+                <ion-icon name="sync" />
+              </ion-button>
             {:else}
-              <ion-icon name="play" />
+              <ion-button {disabled} on:click={live}>
+                <ion-icon color="red" name="radio" />
+              </ion-button>
             {/if}
-          </ion-button>
-          <ion-button {disabled} on:click={skip}>
-            <ion-icon name="play-forward" />
-          </ion-button>
-        </ion-buttons>
+          </ion-buttons>
+        {:else}
+          <ion-thumbnail on:click={showPlayer} slot="start">
+            <SquareImage src={track.artworkM.url} />
+          </ion-thumbnail>
+          <ion-title on:click={showPlayer}>{track.name}</ion-title>
+          <ion-buttons slot="end">
+            <ion-button {disabled} on:click={play_or_pause}>
+              {#if $player.value === "playing"}
+                <ion-icon name="pause" />
+              {:else if $player.value === "loading" || $player.value === "playerSelecting"}
+                <ion-icon name="sync" />
+              {:else}
+                <ion-icon name="play" />
+              {/if}
+            </ion-button>
+            <ion-button {disabled} on:click={skip}>
+              <ion-icon name="play-forward" />
+            </ion-button>
+          </ion-buttons>
+        {/if}
       {/key}
     {:else}
       <ion-thumbnail slot="start">
