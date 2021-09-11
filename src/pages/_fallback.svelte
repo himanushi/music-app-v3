24a7a64da968437d45f7e8306d4f6116c00e9893
himanushi <himanushi@users.noreply.github.com>
2021-09-11 20:07:39 +0900

    404 デザイン適用

--- src/pages/_fallback.svelte
+++ src/pages/_fallback.svelte
@@ -1,1 +1,8 @@
-<div>404</div>
+<ion-card>
+  <ion-card-header>
+    <ion-card-subtitle>Not Found</ion-card-subtitle>
+    <ion-card-title>404</ion-card-title>
+  </ion-card-header>
+
+  <ion-card-content> ページが存在しませんでした </ion-card-content>
+</ion-card>
