aaaed6d876ffaf0e87dcacb40b58161a23a2e406
himanushi <himanushi@users.noreply.github.com>
2021-09-14 05:16:52 +0900

    チェックボックスでもメッセージを表示

--- src/components/input-checkbox.svelte
+++ src/components/input-checkbox.svelte
@@ -1,4 +1,6 @@
 <script>
+import Messages from "./messages.svelte";
+
 export let label = "";
 export let errorMessages: string[] | undefined = undefined;
 export let checked: boolean;
@@ -18,3 +20,6 @@ const onChange = (event: Event) => {
   <ion-label>{label}</ion-label>
   <ion-checkbox color="main" {checked} on:ionChange={onChange} />
 </ion-item>
+{#if errorMessages}
+  <Messages type="error" messages={errorMessages} />
+{/if}
