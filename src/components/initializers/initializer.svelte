957395985542acaff8565d5a143f2a2132ef2018
himanushi <himanushi@users.noreply.github.com>
2021-10-06 19:28:51 +0900

    アプリの場合はクッキーポリシーを確認しない

--- src/components/initializers/initializer.svelte
+++ src/components/initializers/initializer.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+import { Capacitor } from "@capacitor/core";
 import AppleMusicAccount from "~/components/initializers/apple-music-account.svelte";
 import CookiePolicy from "~/components/initializers/cookie-policy.svelte";
 import Version from "~/components/initializers/version.svelte";
@@ -6,4 +7,6 @@ import Version from "~/components/initializers/version.svelte";
 
 <Version />
 <AppleMusicAccount />
-<CookiePolicy />
+{#if Capacitor.getPlatform() === "web"}
+  <CookiePolicy />
+{/if}
