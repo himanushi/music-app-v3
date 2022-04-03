d285d66b87b8e15f88919f9831dfa81318857088
himanushi <himanushi@users.noreply.github.com>
2022-04-03 13:03:08 +0900

    クリップボードコンポーネント追加

---
+++ src/components/clipboard-item.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { Clipboard } from "@capacitor/clipboard";
+import { openToast } from "~/lib/ionic";
+
+export let name: string;
+
+const copy = async () => {
+  await Clipboard.write({
+    string: name,
+  });
+  openToast({
+    color: "light-green",
+    duration: 1000,
+    message: "クリップボードにコピーしました",
+  });
+};
+</script>
+
+<ion-item on:click={copy} button detail={false}>
+  <ion-label class="ion-text-wrap">
+    {name}
+  </ion-label>
+  <ion-icon slot="end" name="clipboard-outline" color="gray" />
+</ion-item>
