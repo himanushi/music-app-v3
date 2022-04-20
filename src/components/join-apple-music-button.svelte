1e27cb644ee99c1986bec990fc02ad0249de9d30
himanushi <himanushi@users.noreply.github.com>
2022-04-20 09:00:55 +0900

    サブスクリプション加入していない時のみ加入ボタンを表示する

--- src/components/join-apple-music-button.svelte
+++ src/components/join-apple-music-button.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+import { CapacitorAppleMusic } from "capacitor-plugin-applemusic";
+
 import isMobile from "ismobilejs";
+import { onMount } from "svelte";
 import { appleAffiliateToken } from "~/lib/variable";
 import { accountService } from "~/machines/apple-music-account-machine";
 
@@ -19,9 +22,15 @@ if (isMobile(window.navigator).apple.device) {
 } else {
   href = `https://music.apple.com/jp/browse?app=music&p=subscribe${token}`;
 }
+
+let hasMusicSubscription = false;
+onMount(async () => {
+  hasMusicSubscription = (await CapacitorAppleMusic.hasMusicSubscription()).
+    result;
+});
 </script>
 
-{#if accountService && $accountService.matches("unauthorized")}
+{#if accountService && ($accountService.matches("unauthorized") || !hasMusicSubscription)}
   <ion-item button class="apple" target="_blank" {href}>
     <ion-icon slot="start" src="/assets/logo-apple-music.svg" />
     <ion-label> Apple Music に加入 </ion-label>
