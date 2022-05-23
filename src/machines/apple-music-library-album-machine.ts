e9d838e4797d3bb48155a33784ecb12de7a72c58
himanushi <himanushi@users.noreply.github.com>
2022-05-23 20:08:37 +0900

    track 読み込み機能追加

--- src/machines/apple-music-library-album-machine.ts
+++ src/machines/apple-music-library-album-machine.ts
@@ -4,21 +4,31 @@
 import { assign, DoneInvokeEvent, interpret, send } from "xstate";
 import { createMachine } from "xstate";
 
+export type LibraryTrack = {
+  libraryId: string;
+  name: string;
+  artworkUrl?: string;
+  purchasedId?: string;
+};
+
 export type LibraryAlbum = {
   libraryId: string;
   name: string;
   artworkUrl?: string;
+  tracks: LibraryTrack[];
 };
 
 type Context = {
   hasNext: boolean;
-  fetchUrl?: string;
-  albumIds: string[];
+  albumFetchUrl?: string;
+  searchAlbumLibraryIds: string[];
   albums: LibraryAlbum[];
   total: number;
 };
 
 const id = "apple-music-library-album";
+const endpoint = "/v1/me/library/albums";
+const limit = 10;
 
 export const libraryAlbumsMachine = createMachine<Context>(
   {
@@ -28,61 +38,97 @@ export const libraryAlbumsMachine = createMachine<Context>(
 
     context: {
       hasNext: true,
-      fetchUrl: "/v1/me/library/albums?limit=100",
+      albumFetchUrl: `${endpoint}?limit=${limit}&include=library-tracks`,
       albums: [],
       total: 0,
-      albumIds: [],
+      searchAlbumLibraryIds: [],
     },
 
     states: {
       // ログイン後に LOAD を実行すること
       idle: {
         on: {
-          LOAD: "loading",
+          LOAD: "checking",
         },
       },
 
-      // 完了するかロードするかチェック
+      // 完了するかアルバムロードするかトラックロードするかチェック
       checking: {
-        entry: send({ type: "LOAD_test" }),
+        entry: send({ type: "LOAD" }),
         on: {
           LOAD: [
-            { target: "loading",
-              cond: (context) => context.hasNext },
+            {
+              target: "tracksLoading",
+              cond: (context) => context.searchAlbumLibraryIds.length > 0,
+            },
+            {
+              target: "albumLoading",
+              cond: (context) => context.hasNext,
+            },
             { target: "done" },
           ],
         },
       },
 
-      loading: {
+      albumLoading: {
         invoke: {
-          id: "getLibraryAlbums",
-
           src: (context) => {
-            if (!context.fetchUrl) {
+            if (!context.albumFetchUrl) {
               throw new Error("fetchUrl が設定されていない！");
             }
-            return MusicKit.getInstance().api.music(context.fetchUrl);
+            return MusicKit.getInstance().api.music(context.albumFetchUrl);
           },
+
+          onDone: {
+            target: "checking",
+            actions: assign({
+              searchAlbumLibraryIds: (
+                _,
+                event: DoneInvokeEvent<MusicKit.APIResult>
+              ) => event.data.data.data.map((album) => album.id),
+              albumFetchUrl: (_, event) => `${event.data.data.next}&limit=${limit}`,
+              // hasNext: (context, event) => event.data.data.meta.total !==
+              //   context.albums.length + event.data.data.data.length,
+              hasNext: (_, _event) => false,
+              total: (_, event) => event.data.data.meta.total,
+            }),
+          },
+          // onError: "checking",
+        },
+        on: {},
+      },
+
+      tracksLoading: {
+        invoke: {
+          src: (context) => MusicKit.getInstance().api.music(
+            `${endpoint}/${context.searchAlbumLibraryIds[0]}`
+          ),
 
           onDone: {
             target: "checking",
             actions: assign({
               albums: (context, event: DoneInvokeEvent<MusicKit.APIResult>) => [
                 ...context.albums,
-                ...event.data.data.data.map((album) => ({
-                  libraryId: album.attributes.playParams.id,
-                  name: album.attributes.name,
-                  artworkUrl: album.attributes.artwork?.url || "/no-image.png",
-                })),
+                ...event.data.data.data.map((album) => {
+                  console.log({ album });
+                  return {
+                    libraryId: album.attributes.playParams.id,
+                    name: album.attributes.name,
+                    artworkUrl:
+                      album.attributes.artwork?.url || "/no-image.png",
+                    tracks: album.relationships.tracks.data.map((track) => ({
+                      libraryId: track.id,
+                      name: track.attributes.name,
+                      artworkUrl: album.attributes.artwork?.url,
+                      purchasedId: track.attributes.playParams?.purchasedId,
+                    })),
+                  };
+                }),
               ],
-              fetchUrl: (_, event) => `${event.data.data.next}&limit=100`,
-              hasNext: (context, event) => event.data.data.meta.total !==
-                context.albums.length + event.data.data.data.length,
-              total: (_, event) => event.data.data.meta.total,
+              searchAlbumLibraryIds: (context, _event) => context.searchAlbumLibraryIds.splice(1),
             }),
           },
-          onError: "checking",
+          // onError: "checking",
         },
         on: {},
       },
