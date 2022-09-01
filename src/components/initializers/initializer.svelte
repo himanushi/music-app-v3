0afe7b708f850bf7d542d7351790472efaea4060
himanushi <himanushi@users.noreply.github.com>
2022-09-02 07:53:39 +0900

    不要な処理削除

--- src/components/initializers/initializer.svelte
+++ src/components/initializers/initializer.svelte
@@ -5,11 +5,9 @@ import AppleMusicNativeAccount from "~/components/initializers/apple-music-nativ
 import AppleMusicWebAccount from "~/components/initializers/apple-music-web-account.svelte";
 import CookiePolicy from "~/components/initializers/cookie-policy.svelte";
 import Version from "~/components/initializers/version.svelte";
-import PreloadLibraryAlbums from "~/components/initializers/preload-library-albums.svelte";
 </script>
 
 <Version />
-<!-- <PreloadLibraryAlbums /> -->
 {#if Capacitor.getPlatform() === "web"}
   <AppleMusicWebAccount />
   <CookiePolicy />
--- src/components/initializers/preload-library-albums.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-import { accountService } from "~/machines/apple-music-account-machine";
-import { libraryAlbumsService } from "~/machines/apple-music-library-album-machine";
-
-$: if ($accountService && $accountService.matches("authorized")) {
-  libraryAlbumsService.send("LOAD");
-}
-</script>
